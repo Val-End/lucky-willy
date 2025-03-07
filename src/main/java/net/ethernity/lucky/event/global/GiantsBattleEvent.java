f4e63b5daed714f30b1b1b3c1dc019050fd1ae2a
Val-End <randomgamer133@gmail.com>
2025-03-07 18:13:48 -0300

    Update GiantsBattleEvent.java

--- src/main/java/net/ethernity/lucky/event/global/GiantsBattleEvent.java
+++ src/main/java/net/ethernity/lucky/event/global/GiantsBattleEvent.java
@@ -4,6 +4,7 @@ import net.ethernity.lucky.entity.effect.LuckyWillyEffects;
 import net.ethernity.lucky.event.LuckyEvent;
 import net.ethernity.lucky.util.ItemUtil;
 import net.minecraft.enchantment.Enchantments;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.attribute.EntityAttributes;
@@ -17,9 +18,13 @@ import net.minecraft.text.Style;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Box;
 import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
 
 public class GiantsBattleEvent extends LuckyEvent {
+    private static final double ATTACK_RANGE = Math.sqrt(2.04F) - 0.6F;
+
     public GiantsBattleEvent() {
         super(-1);
     }
@@ -27,6 +32,23 @@ public class GiantsBattleEvent extends LuckyEvent {
     @Override
     public void execute(BlockPos pos, World world, PlayerEntity player) {
         ZombieEntity bob = new ZombieEntity(world) {
+            @Override
+            protected Box getAttackBox() {
+                Entity entity = this.getVehicle();
+                Box box3;
+                if (entity != null) {
+                    Box box = entity.getBoundingBox();
+                    Box box2 = this.getBoundingBox();
+                    box3 = new Box(
+                            Math.min(box2.minX, box.minX), box2.minY, Math.min(box2.minZ, box.minZ), Math.max(box2.maxX, box.maxX), box2.maxY, Math.max(box2.maxZ, box.maxZ)
+                    );
+                } else {
+                    box3 = this.getBoundingBox();
+                }
+
+                return box3.expand(ATTACK_RANGE * 2, 0.0, ATTACK_RANGE * 2);
+            }
+
             @Override
             public void tick() {
                 super.tick();
